a644167f59b7428cb8402b163353347ff8de781f
F1nk40 <riainar@bk.ru>
2021-09-18 21:21:07 +0300

    directory node has been added

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+using System.IO;
+namespace ConsoleApp8
+{
+    class DirectoryFileInfoHandler {
+
+        private List<DirectoryFileInfo> _directoryFileInfoList = new List<DirectoryFileInfo>();
+        public void AddNewNode(DirectoryFileInfo DFI)
+        {
+            _directoryFileInfoList.Add(DFI);
+        }
+    }
+    class DirectoryFileInfo {
+        public DirectoryFileInfo()
+        {
+
+        }
+        public DirectoryFileInfo Up{
+            get { return}
+        }
+        public DirectoryFileInfo(DirectoryInfo directoryInfos, IEnumerable<FileInfo> fileInfos)
+        {
+
+        }
+
+        List<DirectoryInfo> Directories;
+        List<FileInfo> Files;
+    }
+    class Program
+    {
+        public (IEnumerable<DirectoryInfo>,IEnumerable<FileInfo>) Get
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
